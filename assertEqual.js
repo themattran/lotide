6facd17e18e03621e67f079ec92e73ea73646880
Matthew Tran <m.tran906@gmail.com>
2021-10-21 22:21:48 +0000

    developed version of console.assert from scratch and used emojis

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+//console.assert(actual === expected);
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(7, 1);
+assertEqual("Dog", "Dog");
