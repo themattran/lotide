5e5dbeefcdf2ebffa1a8bd8d6228172f2b76730f
Matthew Tran <m.tran906@gmail.com>
2021-10-29 02:37:44 +0000

    findKey using callback function

---
+++ findKey.js
@@ -0,0 +1,47 @@
+
+  const findKey = function (object, callback) {
+    const objectKeys = Object.keys(object)
+
+
+    for (let objectKey of objectKeys) {
+      const objectValue = object[objectKey];
+      if (callback(objectValue) === true) { //callback(index)is the key?
+        return objectKey;
+      }
+    }
+  }
+
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+
+
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //console.assert(actual === expected);
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma'))
